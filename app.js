920ca3f960ed44722a1972ff69abad79cb16565b
raashid.alam <raashid.alam@ggktech.com>
2018-12-28 16:01:58 +0530

    added filter functionlity

--- app.js
+++ app.js
@@ -16,54 +16,109 @@ const sql = require("./config");
 //Querries
 //For Client-Side Pagination
 app.get("/", (req, res, next) => {
-    var request = new sql.Request();
-    var query = "SELECT * FROM GV_TEST_PERSON_TABLE";
-    request.query(query, (err, response) => {
-        if (err) {
-            console.log("Error while querying database :- " + err);
-            res.status(406).send("Not acceptable");
-        } else {
-            res.status(200).send(response.recordset);
-        }
-    });
+  var request = new sql.Request();
+  var query = "SELECT * FROM GV_TEST_PERSON_TABLE";
+  request.query(query, (err, response) => {
+    if (err) {
+      console.log("Error while querying database :- " + err);
+      res.status(406).send("Not acceptable");
+    } else {
+      res.status(200).send(response.recordset);
+    }
+  });
 });
 
 //For Server-Side Pagination
 app.post("/", (req, res, next) => {
-    var request = new sql.Request();
-    var offset = req.body.pageSize * req.body.pageNumber;
-    var query;
+  var request = new sql.Request();
+  var offset = req.body.pageSize * req.body.pageNumber;
+  var query;
+
+  if (req.body.filterdata) {
     if (req.body.columnName) {
-        if (req.body.sortState === 0) {
-            query = `SELECT * FROM GV_TEST_PERSON_TABLE ORDER BY (SELECT NULL) OFFSET ${offset} ROWS FETCH NEXT ${req.body.pageSize} ROWS ONLY`;
-        } else if (req.body.sortState === 1) {
-            query = `SELECT * FROM GV_TEST_PERSON_TABLE ORDER BY ${req.body.columnName} ASC OFFSET ${offset} ROWS FETCH NEXT ${req.body.pageSize} ROWS ONLY`;
-        } else if (req.body.sortState === 2) {
-            query = `SELECT * FROM GV_TEST_PERSON_TABLE ORDER BY ${req.body.columnName} DESC OFFSET ${offset} ROWS FETCH NEXT ${req.body.pageSize} ROWS ONLY`;
-        }
+      if (req.body.sortState === 0) {
+        query = `SELECT * FROM GV_TEST_PERSON_TABLE where ${
+          req.body.columnName
+        } like '%${
+          req.body.filterdata
+        }%' ORDER BY (SELECT NULL) OFFSET ${offset} ROWS FETCH NEXT ${
+          req.body.pageSize
+        } ROWS ONLY`;
+      } else if (req.body.sortState === 1) {
+        query = `SELECT * FROM GV_TEST_PERSON_TABLE where ${
+          req.body.columnName
+        } like '%${req.body.filterdata}%' ORDER BY ${
+          req.body.columnName
+        } ASC OFFSET ${offset} ROWS FETCH NEXT ${req.body.pageSize} ROWS ONLY`;
+      } else if (req.body.sortState === 2) {
+        query = `SELECT * FROM GV_TEST_PERSON_TABLE where ${
+          req.body.columnName
+        } like '%${req.body.filterdata}%' ORDER BY ${
+          req.body.columnName
+        } DESC OFFSET ${offset} ROWS FETCH NEXT ${req.body.pageSize} ROWS ONLY`;
+      }
     } else {
-        query = `SELECT * FROM GV_TEST_PERSON_TABLE ORDER BY (SELECT NULL) OFFSET ${offset} ROWS FETCH NEXT ${req.body.pageSize} ROWS ONLY`;
+      if (req.body.sortState === 0) {
+        query = `SELECT * FROM GV_TEST_PERSON_TABLE where ${
+          req.body.columnName
+        } like '%${
+          req.body.filterdata
+        }%' ORDER BY (SELECT NULL) OFFSET ${offset} ROWS FETCH NEXT ${
+          req.body.pageSize
+        } ROWS ONLY`;
+      } else if (req.body.sortState === 1) {
+        query = `SELECT * FROM GV_TEST_PERSON_TABLE where ${
+          req.body.columnName
+        } like '%${req.body.filterdata}%' ORDER BY ${
+          req.body.columnName
+        } ASC OFFSET ${offset} ROWS FETCH NEXT ${req.body.pageSize} ROWS ONLY`;
+      } else if (req.body.sortState === 2) {
+        query = `SELECT * FROM GV_TEST_PERSON_TABLE where ${
+          req.body.columnName
+        } like '%${req.body.filterdata}%' ORDER BY ${
+          req.body.columnName
+        } DESC OFFSET ${offset} ROWS FETCH NEXT ${req.body.pageSize} ROWS ONLY`;
+      }
     }
-    request.query(query, (err, response) => {
-        console.log(query);
-        if (err) {
-            console.log("Error while querying database :- " + err);
-            res.status(406).send("Not acceptable");
-        } else {
-            res.status(200).send(response.recordset);
-        }
-    });
+  } else if (req.body.columnName) {
+    if (req.body.sortState === 0) {
+      query = `SELECT * FROM GV_TEST_PERSON_TABLE ORDER BY (SELECT NULL) OFFSET ${offset} ROWS FETCH NEXT ${
+        req.body.pageSize
+      } ROWS ONLY`;
+    } else if (req.body.sortState === 1) {
+      query = `SELECT * FROM GV_TEST_PERSON_TABLE ORDER BY ${
+        req.body.columnName
+      } ASC OFFSET ${offset} ROWS FETCH NEXT ${req.body.pageSize} ROWS ONLY`;
+    } else if (req.body.sortState === 2) {
+      query = `SELECT * FROM GV_TEST_PERSON_TABLE ORDER BY ${
+        req.body.columnName
+      } DESC OFFSET ${offset} ROWS FETCH NEXT ${req.body.pageSize} ROWS ONLY`;
+    }
+  } else {
+    query = `SELECT * FROM GV_TEST_PERSON_TABLE ORDER BY (SELECT NULL) OFFSET ${offset} ROWS FETCH NEXT ${
+      req.body.pageSize
+    } ROWS ONLY`;
+  }
+  request.query(query, (err, response) => {
+    console.log(query);
+    if (err) {
+      console.log("Error while querying database :- " + err);
+      res.status(406).send("Not acceptable");
+    } else {
+      res.status(200).send(response.recordset);
+    }
+  });
 });
 
 //Default errors
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message,
-            status: error.status
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+      status: error.status
+    }
+  });
 });
 
 //Export app
